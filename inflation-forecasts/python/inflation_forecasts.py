b3921948e88a1f711919602556c98d52ba89b5fb
Brian Jenkins <letsgoexploring@gmail.com>
2019-06-20 15:08:24 -0700

    major updates

---
+++ inflation-forecasts/python/inflation_forecasts.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.dates as dts
+import pandas as pd
+import fredpy as fp
+import datetime,dateutil,urllib,runProcs
+import requests
+import matplotlib.pyplot as plt
+plt.style.use('classic')
+# get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[2]:
+
+
+# 1. Import the most recent inflation forecast data from the Philadelphia Fed, Survey of Professional Forecasters
+
+url = "https://www.philadelphiafed.org/-/media/research-and-data/real-time-center/survey-of-professional-forecasters/historical-data/inflation.xls?la=en"
+r = requests.get(url,verify=False)
+with open("../xls/inflation_forecasts.xls", "wb") as code:
+    code.write(r.content)
+
+# dls = "http://www.philadelphiafed.org/research-and-data/real-time-center/survey-of-professional-forecasters/historical-data/inflation.xls"
+# urllib.urlretrieve(dls, "inflation_forecasts.xls")
+
+
+# In[3]:
+
+
+# 2. Download and manage data from FRED
+gdp_deflator_Q=fp.series('GDPDEF')
+gdp_deflator_A=fp.series('A191RD3A086NBEA')
+gdp_deflator_Q = gdp_deflator_Q.apc(method='forward')
+gdp_deflator_A = gdp_deflator_A.apc(method='forward')
+gdp_deflator_Q = gdp_deflator_Q.window(['07-01-1970','01-01-2200'])
+gdp_deflator_A = gdp_deflator_A.window(['07-01-1970','01-01-2200'])
+
+interest_Q = fp.series('GS1')
+interest_A = fp.series('GS1')
+
+interest_Q = interest_Q.as_frequency(freq='Q',method='mean')
+interest_A = interest_A.as_frequency(freq='A',method='mean')
+
+interest_Q = interest_Q.window(['07-01-1970','01-01-2200'])
+interest_A = interest_A.window(['07-01-1970','01-01-2200'])
+
+
+# In[4]:
+
+
+# 3. Create forecast series as FRED objects
+
+# 3.1 import the inflation forecasts from Excel file and fill in missing value for 1974:Q3
+inflation_forecasts = pd.read_excel('../xls/inflation_forecasts.xls')
+inflation_forecasts['INFPGDP1YR']=inflation_forecasts['INFPGDP1YR'].interpolate()
+
+# 3.2 initialize some FRED objects
+# gdp_deflator_forecast_Q=fp.series('GDPDEF')
+# gdp_deflator_forecast_A=fp.series('GDPDEF')
+
+# 3.3 Associate forecasts with dates. The date should coincide with the start of the period for which the forecast applies.
+dates = []
+for i,ind in enumerate(inflation_forecasts.index):
+    year =int(inflation_forecasts.iloc[i]['YEAR'])
+    quart=int(inflation_forecasts.iloc[i]['QUARTER'])
+    if quart == 1:
+        month = '04'
+    elif quart == 2:
+        month = '07'
+    elif quart == 3:
+        month = '10'
+    else:
+        month = '01'
+        year=year+1
+    date = month+'-01-'+str(year)
+    dates.append(date)
+# dateNumbers = [dateutil.parser.parse(s) for s in dates]
+
+# # 3.4 Create the FRED objects
+gdp_deflator_forecast_Q = fp.to_fred_series(data = inflation_forecasts['INFPGDP1YR'].values,dates=dates,frequency_short='Q')
+
+gdp_deflator_forecast_A = fp.to_fred_series(data = inflation_forecasts['INFPGDP1YR'].values,dates=dates,frequency_short='A')
+gdp_deflator_forecast_A = gdp_deflator_forecast_A.as_frequency(freq='A',method='mean')
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+# 3.5 Create data frames with forecast inflation, actual inflation, and the 1-year bond rate
+gdp_deflator_Q,gdp_deflator_forecast_Q,interest_Q = fp.window_equalize([gdp_deflator_Q,gdp_deflator_forecast_Q,interest_Q])
+gdp_deflator_A,gdp_deflator_forecast_A,interest_A = fp.window_equalize([gdp_deflator_A,gdp_deflator_forecast_A,interest_A])
+inflation_forecast_Q_df=pd.DataFrame({'1-year inflation forecast':gdp_deflator_forecast_Q.data,'1-year actual inflation':gdp_deflator_Q.data,'1-year nominal interest rate':interest_Q.data})
+inflation_forecast_A_df=pd.DataFrame({'1-year inflation forecast':gdp_deflator_forecast_A.data,'1-year actual inflation':gdp_deflator_A.data,'1-year nominal interest rate':interest_A.data})
+
+
+# In[6]:
+
+
+# 3.6 Save data to csv
+inflation_forecast_Q_df.to_csv('../csv/inflation_forecastsQ.csv',index=True,index_label='date')
+inflation_forecast_A_df.to_csv('../csv/inflation_forecastsA.csv',index=True,index_label='date')
+
+
+# In[7]:
+
+
+# 4. Plot some things
+
+# 4.1 actual inflation, expected inflation, 1-year interest rate: quarterly
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+ax.plot(gdp_deflator_Q.data,'b-',lw=3)
+ax.plot(gdp_deflator_forecast_Q.data,'r--',lw=3)
+ax.plot(interest_Q.data,'m-.',lw=3)
+ax.set_title('Quarterly')
+ax.set_xlabel('Date')
+ax.set_ylabel('%')
+ax.legend(['actual $\pi$','forecast $\pi$','interest'],loc='upper right')
+# interest_Q.recessions()
+plt.grid()
+
+
+# In[8]:
+
+
+# 4.2 actual inflation, expected inflation, 1-year interest rate: annual
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+ax.plot(gdp_deflator_A.data,'b-o',lw=3)
+ax.plot(gdp_deflator_forecast_A.data,'r--o',lw=3)
+ax.plot(interest_A.data,'m-.o',lw=3)
+ax.set_title('Annual')
+ax.set_xlabel('Date')
+ax.set_ylabel('%')
+ax.legend(['actual $\pi$','forecast $\pi$','interest'],loc='upper right')
+# interest_A.recessions()
+plt.grid()
+
+
+# In[9]:
+
+
+# 5. Real interest rates
+
+# 5.1 Construct real interest rate series: ex ante and ex post
+real_ex_ante_A = interest_A.data - gdp_deflator_forecast_A.data
+real_ex_post_A = interest_A.data - gdp_deflator_A.data
+real_ex_ante_Q = interest_Q.data - gdp_deflator_forecast_Q.data
+real_ex_post_Q = interest_Q.data - gdp_deflator_Q.data
+
+
+# In[10]:
+
+
+# 5.2 ex ante and ex post real interest rates: annual
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+ax.plot(real_ex_ante_A,'b-o',lw=3)
+ax.plot(real_ex_post_A,'r--o',lw=3)
+ax.set_title('Annual real interest rate')
+ax.set_xlabel('Date')
+ax.set_ylabel('%')
+ax.legend(['ex ante','ex post'],loc='upper right')
+# interest_A.recessions()
+plt.grid()
+
+
+# In[11]:
+
+
+# 5.2 ex ante and ex post real interest rates: quarterly
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+ax.plot(real_ex_ante_Q,'b-',lw=3)
+ax.plot(real_ex_post_Q,'r--',lw=3)
+ax.set_title('Quarterly real interest rate')
+ax.set_xlabel('Date')
+ax.set_ylabel('%')
+ax.legend(['ex ante','ex post'],loc='upper right')
+# interest_Q.recessions()
+plt.grid()
+
+
+# In[12]:
+
+
+# # 6. Consumption Euler equation
+
+# 6.1 create the consumption series
+cons=fp.series('PCECA')
+defl=fp.series('A191RD3A086NBEA')
+cons,defl = fp.window_equalize([cons,defl])
+
+cons = cons.pc(method='backward')
+interest_A,cons = fp.window_equalize([interest_A,cons])
+
+
+# In[13]:
+
+
+# 6.2 Predicted real interest rate: sigma = 1
+sigma = 1
+beta = .98
+gc=np.mean(cons.data)
+
+r_pred_A = sigma*np.array(cons.data - np.mean(cons.data)) - 100*np.log(beta)
+print(gc)
+
+
+# In[14]:
+
+
+r_pred_A
+
+
+# In[15]:
+
+
+# 6.3 Plot the predicted real interest rate
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+ax.plot(real_ex_ante_A,'b-',lw=3)
+ax.plot(real_ex_ante_A.index,r_pred_A,'r--',lw=3)
+ax.set_title('Annual ex ante real interest rate')
+ax.set_xlabel('Date')
+ax.set_ylabel('%')
+ax.legend(['actual','predicted'],loc='upper right')
+# interest_A.recessions()
+plt.grid()
+
+
+# In[16]:
+
+
+np.corrcoef(cons.data, real_ex_ante_A)
+
+
+# In[17]:
+
+
+# 7. Export to notebook to .py
+runProcs.exportNb('inflation_forecasts')
+
---
+++ quantity-theory/python/quantity_theory_data.py
@@ -0,0 +1,581 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('classic')
+import pandas as pd
+import quandl as Quandl
+import wbdata as wb
+from scipy import stats
+import runProcs
+# get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# # Preliminaries
+# 
+# Import country codes and country lists by income
+
+# In[2]:
+
+
+# 1. Import country codes and organize
+
+# 1.1 Import country codes and names from the country_codes file from Quandl's WB WDI documentation: https://www.quandl.com/data/WWDI/documentation/documentation
+country_codes = {}
+
+try:
+    text_file = open('country_codes', 'r')
+    lines = text_file.readlines()
+    for line in lines:
+        split = line.split('|')
+        if len(split)>1:
+            if len(split[1])==4:
+                country_codes[split[0]] = split[1][:-1]
+except:
+    country_codes = {
+ 'Afghanistan': 'AFG',
+ 'Africa': 'AFR',
+ 'Albania': 'ALB',
+ 'Algeria': 'DZA',
+ 'American Samoa': 'ASM',
+ 'Andorra': 'AND',
+ 'Angola': 'AGO',
+ 'Antigua and Barbuda': 'ATG',
+ 'Arab World': 'ARB',
+ 'Argentina': 'ARG',
+ 'Armenia': 'ARM',
+ 'Aruba': 'ABW',
+ 'Australia': 'AUS',
+ 'Austria': 'AUT',
+ 'Azerbaijan': 'AZE',
+ 'Bahamas, The': 'BHS',
+ 'Bahrain': 'BHR',
+ 'Bangladesh': 'BGD',
+ 'Barbados': 'BRB',
+ 'Belarus': 'BLR',
+ 'Belgium': 'BEL',
+ 'Belize': 'BLZ',
+ 'Benin': 'BEN',
+ 'Bermuda': 'BMU',
+ 'Bhutan': 'BTN',
+ 'Bolivia': 'BOL',
+ 'Bosnia and Herzegovina': 'BIH',
+ 'Botswana': 'BWA',
+ 'Brazil': 'BRA',
+ 'Brunei Darussalam': 'BRN',
+ 'Bulgaria': 'BGR',
+ 'Burkina Faso': 'BFA',
+ 'Burundi': 'BDI',
+ 'Cabo Verde': 'CPV',
+ 'Cambodia': 'KHM',
+ 'Cameroon': 'CMR',
+ 'Canada': 'CAN',
+ 'Caribbean small states': 'CSS',
+ 'Cayman Islands': 'CYM',
+ 'Central African Republic': 'CAF',
+ 'Chad': 'TCD',
+ 'Channel Islands': 'CHI',
+ 'Chile': 'CHL',
+ 'China': 'CHN',
+ 'Colombia': 'COL',
+ 'Comoros': 'COM',
+ 'Congo, Dem. Rep.': 'COD',
+ 'Congo, Rep.': 'COG',
+ 'Costa Rica': 'CRI',
+ "Cote d'Ivoire": 'CIV',
+ 'Croatia': 'HRV',
+ 'Cuba': 'CUB',
+ 'Curacao': 'CUW',
+ 'Cyprus': 'CYP',
+ 'Czech Republic': 'CZE',
+ 'Denmark': 'DNK',
+ 'Djibouti': 'DJI',
+ 'Dominica': 'DMA',
+ 'Dominican Republic': 'DOM',
+ 'East Asia & Pacific (all income levels)': 'EAS',
+ 'East Asia & Pacific (developing only)': 'EAP',
+ 'East Asia and the Pacific (IFC classification)': 'CEA',
+ 'Ecuador': 'ECU',
+ 'Egypt, Arab Rep.': 'EGY',
+ 'El Salvador': 'SLV',
+ 'Equatorial Guinea': 'GNQ',
+ 'Eritrea': 'ERI',
+ 'Estonia': 'EST',
+ 'Ethiopia': 'ETH',
+ 'Euro area': 'EMU',
+ 'Europe & Central Asia (all income levels)': 'ECS',
+ 'Europe & Central Asia (developing only)': 'ECA',
+ 'Europe and Central Asia (IFC classification)': 'CEU',
+ 'European Union': 'EUU',
+ 'Faeroe Islands': 'FRO',
+ 'Fiji': 'FJI',
+ 'Finland': 'FIN',
+ 'France': 'FRA',
+ 'French Polynesia': 'PYF',
+ 'Gabon': 'GAB',
+ 'Gambia, The': 'GMB',
+ 'Georgia': 'GEO',
+ 'Germany': 'DEU',
+ 'Ghana': 'GHA',
+ 'Greece': 'GRC',
+ 'Greenland': 'GRL',
+ 'Grenada': 'GRD',
+ 'Guam': 'GUM',
+ 'Guatemala': 'GTM',
+ 'Guinea': 'GIN',
+ 'Guinea-Bissau': 'GNB',
+ 'Guyana': 'GUY',
+ 'Haiti': 'HTI',
+ 'Heavily indebted poor countries (HIPC)': 'HPC',
+ 'High income': 'HIC',
+ 'High income: OECD': 'OEC',
+ 'High income: nonOECD': 'NOC',
+ 'Honduras': 'HND',
+ 'Hong Kong SAR, China': 'HKG',
+ 'Hungary': 'HUN',
+ 'Iceland': 'ISL',
+ 'India': 'IND',
+ 'Indonesia': 'IDN',
+ 'Iran, Islamic Rep.': 'IRN',
+ 'Iraq': 'IRQ',
+ 'Ireland': 'IRL',
+ 'Isle of Man': 'IMN',
+ 'Israel': 'ISR',
+ 'Italy': 'ITA',
+ 'Jamaica': 'JAM',
+ 'Japan': 'JPN',
+ 'Jordan': 'JOR',
+ 'Kazakhstan': 'KAZ',
+ 'Kenya': 'KEN',
+ 'Kiribati': 'KIR',
+ 'Korea, Dem. Rep.': 'PRK',
+ 'Korea, Rep.': 'KOR',
+ 'Kosovo': 'KSV',
+ 'Kuwait': 'KWT',
+ 'Kyrgyz Republic': 'KGZ',
+ 'Lao PDR': 'LAO',
+ 'Latin America & Caribbean (all income levels)': 'LCN',
+ 'Latin America & Caribbean (developing only)': 'LAC',
+ 'Latin America and the Caribbean (IFC classification)': 'CLA',
+ 'Latvia': 'LVA',
+ 'Least developed countries: UN classification': 'LDC',
+ 'Lebanon': 'LBN',
+ 'Lesotho': 'LSO',
+ 'Liberia': 'LBR',
+ 'Libya': 'LBY',
+ 'Liechtenstein': 'LIE',
+ 'Lithuania': 'LTU',
+ 'Low & middle income': 'LMY',
+ 'Low income': 'LIC',
+ 'Lower middle income': 'LMC',
+ 'Luxembourg': 'LUX',
+ 'Macao SAR, China': 'MAC',
+ 'Macedonia, FYR': 'MKD',
+ 'Madagascar': 'MDG',
+ 'Malawi': 'MWI',
+ 'Malaysia': 'MYS',
+ 'Maldives': 'MDV',
+ 'Mali': 'MLI',
+ 'Malta': 'MLT',
+ 'Marshall Islands': 'MHL',
+ 'Mauritania': 'MRT',
+ 'Mauritius': 'MUS',
+ 'Mexico': 'MEX',
+ 'Micronesia, Fed. Sts.': 'FSM',
+ 'Middle East & North Africa (all income levels)': 'MEA',
+ 'Middle East & North Africa (developing only)': 'MNA',
+ 'Middle East and North Africa (IFC classification)': 'CME',
+ 'Middle income': 'MIC',
+ 'Moldova': 'MDA',
+ 'Monaco': 'MCO',
+ 'Mongolia': 'MNG',
+ 'Montenegro': 'MNE',
+ 'Morocco': 'MAR',
+ 'Mozambique': 'MOZ',
+ 'Myanmar': 'MMR',
+ 'Namibia': 'NAM',
+ 'Nepal': 'NPL',
+ 'Netherlands': 'NLD',
+ 'New Caledonia': 'NCL',
+ 'New Zealand': 'NZL',
+ 'Nicaragua': 'NIC',
+ 'Niger': 'NER',
+ 'Nigeria': 'NGA',
+ 'North Africa': 'NAF',
+ 'North America': 'NAC',
+ 'Northern Mariana Islands': 'MNP',
+ 'Norway': 'NOR',
+ 'OECD members': 'OED',
+ 'Oman': 'OMN',
+ 'Other small states': 'OSS',
+ 'Pacific island small states': 'PSS',
+ 'Pakistan': 'PAK',
+ 'Palau': 'PLW',
+ 'Panama': 'PAN',
+ 'Papua New Guinea': 'PNG',
+ 'Paraguay': 'PRY',
+ 'Peru': 'PER',
+ 'Philippines': 'PHL',
+ 'Poland': 'POL',
+ 'Portugal': 'PRT',
+ 'Puerto Rico': 'PRI',
+ 'Qatar': 'QAT',
+ 'Romania': 'ROU',
+ 'Russian Federation': 'RUS',
+ 'Rwanda': 'RWA',
+ 'Samoa': 'WSM',
+ 'San Marino': 'SMR',
+ 'Sao Tome and Principe': 'STP',
+ 'Saudi Arabia': 'SAU',
+ 'Senegal': 'SEN',
+ 'Serbia': 'SRB',
+ 'Seychelles': 'SYC',
+ 'Sierra Leone': 'SLE',
+ 'Singapore': 'SGP',
+ 'Sint Maarten (Dutch part)': 'SXM',
+ 'Slovak Republic': 'SVK',
+ 'Slovenia': 'SVN',
+ 'Small states': 'SST',
+ 'Solomon Islands': 'SLB',
+ 'Somalia': 'SOM',
+ 'South Africa': 'ZAF',
+ 'South Asia': 'SAS',
+ 'South Asia (IFC classification)': 'CSA',
+ 'South Sudan': 'SSD',
+ 'Spain': 'ESP',
+ 'Sri Lanka': 'LKA',
+ 'St. Kitts and Nevis': 'KNA',
+ 'St. Lucia': 'LCA',
+ 'St. Martin (French part)': 'MAF',
+ 'St. Vincent and the Grenadines': 'VCT',
+ 'Sub-Saharan Africa (IFC classification)': 'CAA',
+ 'Sub-Saharan Africa (all income levels)': 'SSF',
+ 'Sub-Saharan Africa (developing only)': 'SSA',
+ 'Sub-Saharan Africa excluding South Africa': 'SXZ',
+ 'Sub-Saharan Africa excluding South Africa and Nigeria': 'XZN',
+ 'Sudan': 'SDN',
+ 'Suriname': 'SUR',
+ 'Swaziland': 'SWZ',
+ 'Sweden': 'SWE',
+ 'Switzerland': 'CHE',
+ 'Syrian Arab Republic': 'SYR',
+ 'Tajikistan': 'TJK',
+ 'Tanzania': 'TZA',
+ 'Thailand': 'THA',
+ 'Timor-Leste': 'TLS',
+ 'Togo': 'TGO',
+ 'Tonga': 'TON',
+ 'Trinidad and Tobago': 'TTO',
+ 'Tunisia': 'TUN',
+ 'Turkey': 'TUR',
+ 'Turkmenistan': 'TKM',
+ 'Turks and Caicos Islands': 'TCA',
+ 'Tuvalu': 'TUV',
+ 'Uganda': 'UGA',
+ 'Ukraine': 'UKR',
+ 'United Arab Emirates': 'ARE',
+ 'United Kingdom': 'GBR',
+ 'United States': 'USA',
+ 'Upper middle income': 'UMC',
+ 'Uruguay': 'URY',
+ 'Uzbekistan': 'UZB',
+ 'Vanuatu': 'VUT',
+ 'Venezuela, RB': 'VEN',
+ 'Vietnam': 'VNM',
+ 'Virgin Islands (U.S.)': 'VIR',
+ 'West Bank and Gaza': 'PSE',
+ 'World': 'WLD',
+ 'Yemen, Rep.': 'YEM',
+ 'Zambia': 'ZMB',
+ 'Zimbabwe': 'ZWE'}
+    
+        
+#1.2 Use wbdata to get lists of country codes by income groups
+countries_income_all =  [i['id'] for i in wb.get_country(incomelevel=['LIC','MIC','HIC'],display=False)]
+countries_income_H = [i['id'] for i in wb.get_country(incomelevel=['HIC'],display=False)]
+countries_income_M = [i['id'] for i in wb.get_country(incomelevel=['MIC'],display=False)]
+countries_income_L = [i['id'] for i in wb.get_country(incomelevel=['LIC'],display=False)]
+
+countries_income_oecd = ['AUS','CAN','CHL','CZE','DNK','EST','HUN','ISL','ISR','JPN'
+                       ,'KOR','NZL','NOR''POL','SVK','SVN','SWE','CHE','USA']
+
+
+# # Import data from Quandl
+
+# In[3]:
+
+
+# 2. Import data from Quandl
+
+# 2.1 Money supply (LCU)
+money_df = pd.DataFrame({})
+
+for name,key in country_codes.items():
+    try:
+        df = Quandl.get('WWDI/'+key+'_FM_LBL_MONY_CN',authtoken="QqLL1AFCjc31_MVo4qsU")
+        df.columns = [key]
+        money_df = pd.concat([money_df,df],axis=1)
+    except:
+        pass
+
+# 2.2 GDP deflator
+deflator_df = pd.DataFrame({})
+
+for name,key in country_codes.items():
+    try:
+        df = Quandl.get('WWDI/'+key+'_NY_GDP_DEFL_ZS',authtoken="QqLL1AFCjc31_MVo4qsU")
+        df.columns = [key]
+        deflator_df = pd.concat([deflator_df,df],axis=1)
+    except:
+        pass
+
+# 2.3 Real GDP
+gdp_df = pd.DataFrame({})
+
+for name,key in country_codes.items():
+    try:
+        df = Quandl.get('WWDI/'+key+'_NY_GDP_MKTP_KD',authtoken="QqLL1AFCjc31_MVo4qsU")
+        df.columns = [key]
+        gdp_df = pd.concat([gdp_df,df],axis=1)
+    except:
+        pass
+
+# 2.4 Exahange rate relative to USD
+exchange_df = pd.DataFrame({})
+
+for name,key in country_codes.items():
+    try:
+        df = Quandl.get('WWDI/'+key+'_PA_NUS_FCRF',authtoken="QqLL1AFCjc31_MVo4qsU")
+        df.columns = [key]
+        exchange_df = pd.concat([exchange_df,df],axis=1)
+    except:
+        pass
+
+# 2.5 Nominal interest rate (lending rate)
+interest_df = pd.DataFrame({})
+
+for name,key in country_codes.items():
+    try:
+        df = Quandl.get('WWDI/'+key+'_FR_INR_LEND',authtoken="QqLL1AFCjc31_MVo4qsU")
+        df.columns = [key]
+        interest_df = pd.concat([interest_df,df],axis=1)
+    except:
+        pass
+
+
+# # Create data sets: money, prices, and output
+
+# In[4]:
+
+
+# 3. Create datasets for quantity theory without interest and exchange rates
+
+# 3.1 data_frames to use
+data_frames = [money_df, deflator_df, gdp_df]
+
+# 3.2 Identify the codes for countries with at least 10 years of consecutive data for each series
+available_codes = []
+for code in country_codes.values():
+    if all(code in frame for frame in data_frames):
+        if any( all(np.isnan(x) for x in frame[code]) for frame in data_frames):
+            print(code)
+        else:
+            available_codes.append(code)        
+            
+print('Number of countries: ',len(available_codes))
+
+# 3.3 Construct the dataset including the average growth rates of variabes for each country
+included_codes = []
+obs = []
+m_data = []
+p_data = []
+y_data = []
+    
+for c in available_codes:
+    count = 0
+    ind = []
+    
+    for i in data_frames[0].index[1:]:
+
+        noneNan = all( not np.isnan(frame[c].loc[i]) for frame in data_frames)
+        anyNan = any( np.isnan(frame[c].loc[i]) for frame in data_frames)
+
+        if noneNan:
+            count+=1
+            ind.append(i)
+        elif anyNan and count>0:
+            break
+
+    if count >9:
+        m = (money_df[c].loc[ind[-1]]/money_df[c].loc[ind[0]])**(1/(count-1))-1
+        p = (deflator_df[c].loc[ind[-1]]/deflator_df[c].loc[ind[0]])**(1/(count-1))-1
+        y = (gdp_df[c].loc[ind[-1]]/gdp_df[c].loc[ind[0]])**(1/(count-1))-1
+
+        included_codes.append(c)
+        m_data.append(np.around(m,5))
+        p_data.append(np.around(p,5))
+        y_data.append(np.around(y,5))
+        obs.append(count)
+
+
+# 3.3 Identify the names of the countries that are incuded
+included_names = []
+for c in included_codes:
+    for name, code in country_codes.items():
+        if c == code:
+            included_names.append(name)
+
+# 3.4 Create the main dataframe
+quantity_theory_data = pd.DataFrame({'iso code':included_codes,'observations':obs,'inflation':p_data,'money growth':m_data,'gdp growth':y_data},index = included_names)
+quantity_theory_data = quantity_theory_data[['iso code','observations','inflation','money growth','gdp growth']]
+quantity_theory_data = quantity_theory_data.sort_index()
+
+# 3.5 Create data_frames organized by income levels
+index_L=[]
+index_M=[]
+index_H=[]
+index_oecd=[]
+
+for country in quantity_theory_data.index:
+    code = quantity_theory_data['iso code'].loc[country]
+    if code in countries_income_L:
+        index_L.append(country)
+    if code in countries_income_M:
+        index_M.append(country)
+    if code in countries_income_H:
+        index_H.append(country)
+    if code in countries_income_oecd:
+        index_oecd.append(country)
+
+# Drop countries with inf values
+quantity_theory_data = quantity_theory_data.replace([np.inf, -np.inf], np.nan).dropna()
+
+quantity_theory_data_L = quantity_theory_data.loc[index_L]
+quantity_theory_data_M = quantity_theory_data.loc[index_M]
+quantity_theory_data_H = quantity_theory_data.loc[index_H]
+quantity_theory_data_oecd = quantity_theory_data.loc[index_oecd]
+
+# 3.6 Export data_frames to csv
+quantity_theory_data.to_csv('quantity_theory_data.csv',index=True,index_label='country')
+quantity_theory_data_L.to_csv('quantity_theory_data_L.csv',index=True,index_label='country')
+quantity_theory_data_M.to_csv('quantity_theory_data_M.csv',index=True,index_label='country')
+quantity_theory_data_H.to_csv('quantity_theory_data_H.csv',index=True,index_label='country')
+quantity_theory_data_oecd.to_csv('quantity_theory_data_oecd.csv',index=True,index_label='country')
+
+
+# # Create data sets: money, prices, output, interest, and exchange rates
+
+# In[5]:
+
+
+# 4. Create datasets for quantity theory with interest and exchange rates
+
+# 4.1 data_frames to use
+data_frames = [money_df, deflator_df, gdp_df,interest_df,exchange_df]
+
+# 4.2 Identify the codes for countries with at leaset 10 years of consecutive data for each series
+available_codes = []
+for code in country_codes.values():
+    if all(code in frame for frame in data_frames):
+        if any( all(np.isnan(x) for x in frame[code]) for frame in data_frames):
+            print(code)
+        else:
+            available_codes.append(code)        
+            
+print('Number of countries: ',len(available_codes))
+
+# 4.3 Construct the dataset including the average growth rates of variabes for each country
+included_codes = []
+obs = []
+m_data = []
+p_data = []
+y_data = []
+i_data = []
+e_data = []
+    
+for c in available_codes:
+    count = 0
+    ind = []
+    
+    for i in data_frames[0].index[1:]:
+
+        noneNan = all( not np.isnan(frame[c].loc[i]) for frame in data_frames)
+        anyNan = any( np.isnan(frame[c].loc[i]) for frame in data_frames)
+
+        if noneNan:
+            count+=1
+            ind.append(i)
+        elif anyNan and count>0:
+            break
+
+    if count >9:
+        m = (money_df[c].loc[ind[-1]]/money_df[c].loc[ind[0]])**(1/(count-1))-1
+        p = (deflator_df[c].loc[ind[-1]]/deflator_df[c].loc[ind[0]])**(1/(count-1))-1
+        y = (gdp_df[c].loc[ind[-1]]/gdp_df[c].loc[ind[0]])**(1/(count-1))-1
+        e = (exchange_df[c].loc[ind[-1]]/exchange_df[c].loc[ind[0]])**(1/(count-1))-1
+        rate = np.mean(interest_df[c].iloc[1:])/100
+
+        included_codes.append(c)
+        m_data.append(np.around(m,5))
+        p_data.append(np.around(p,5))
+        y_data.append(np.around(y,5))
+        e_data.append(np.around(e,5))
+        i_data.append(np.around(rate,5))
+        obs.append(count)
+
+
+# 4.3 Identify the names of the countries that are incuded
+included_names = []
+for c in included_codes:
+    for name, code in country_codes.items():
+        if c == code:
+            included_names.append(name)
+
+# 4.4 Create the main dataframe
+quantity_theory_data = pd.DataFrame({'iso code':included_codes,'observations':obs,'inflation':p_data,'money growth':m_data,'gdp growth':y_data,'nominal interest rate':i_data,'exchange rate depreciation':e_data},index = included_names)
+quantity_theory_data = quantity_theory_data[['iso code','observations','inflation','money growth','gdp growth','nominal interest rate','exchange rate depreciation']]
+quantity_theory_data = quantity_theory_data.sort_index()
+
+# 4.5 Create data_frames organized by income levels
+index_L=[]
+index_M=[]
+index_H=[]
+index_oecd=[]
+
+for country in quantity_theory_data.index:
+    code = quantity_theory_data['iso code'].loc[country]
+    if code in countries_income_L:
+        index_L.append(country)
+    if code in countries_income_M:
+        index_M.append(country)
+    if code in countries_income_H:
+        index_H.append(country)
+    if code in countries_income_oecd:
+        index_oecd.append(country)
+        
+# Drop countries with inf values
+quantity_theory_data = quantity_theory_data.replace([np.inf, -np.inf], np.nan).dropna()
+        
+quantity_theory_data_L = quantity_theory_data.loc[index_L]
+quantity_theory_data_M = quantity_theory_data.loc[index_M]
+quantity_theory_data_H = quantity_theory_data.loc[index_H]
+quantity_theory_data_oecd = quantity_theory_data.loc[index_oecd]
+
+# 4.6 Export data_frames to csv
+quantity_theory_data.to_csv('qty_theory_open_data.csv',index=True,index_label='country')
+quantity_theory_data_L.to_csv('qty_theory_open_data_L.csv',index=True,index_label='country')
+quantity_theory_data_M.to_csv('qty_theory_open_data_M.csv',index=True,index_label='country')
+quantity_theory_data_H.to_csv('qty_theory_open_data_H.csv',index=True,index_label='country')
+quantity_theory_data_oecd.to_csv('qty_theory_open_dataOecd.csv',index=True,index_label='country')
+
+
+# In[ ]:
+
+
+# 5. Export notebook to python script
+runProcs.exportNb('quantityTheory_data')
+
